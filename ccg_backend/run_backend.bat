ff055f321f894a086858596b295d36cb76918276
Miskat Mahmud <115903852+codedsami@users.noreply.github.com>
2025-01-29 15:19:34 -0500

    Update run_backend.bat

--- ccg_backend/run_backend.bat
+++ ccg_backend/run_backend.bat
@@ -1,8 +1,10 @@
 @echo off
 if "%CONDA_DEFAULT_ENV%"=="myenv" (
     cd docker-configs
+    docker pull postgis/postgis
     docker-compose up -d
     cd ..
+    timeout /T 5 /NOBREAK
     python manage.py runserver 0.0.0.0:8000    
 ) else (
     echo Please run "conda activate myenv"
