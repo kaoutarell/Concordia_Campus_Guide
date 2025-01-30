f75fe3d0574f4e9f6bbf5700cf189e010e94ca4c
Zeiad Badawy <z_badawy@live.concordia.ca>
2025-01-30 13:45:50 -0500

    run_backend script for macos

---
+++ ccg_backend/run_backend.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ "$CONDA_DEFAULT_ENV" == "myenv" ]; then
+    cd docker-configs
+    docker pull postgis/postgis
+    docker-compose up -d
+    cd ..
+    sleep 5
+    python manage.py makemigrations
+    python manage.py migrate
+    python manage.py loaddata buildings.json
+    python manage.py loaddata routes.json
+    python manage.py loaddata shuttle_stops.json
+    python manage.py runserver 0.0.0.0:8000    
+else
+    echo 'Please run "conda activate myenv"'
+    exit 1
+fi
