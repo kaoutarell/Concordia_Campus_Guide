5b71805b151a127f1ad1b0d55f61758888b5fa32
Vega64 <omar.hammodan@gmail.com>
2025-02-08 10:49:22 -0500

    added marker for start and finish as well as path between
    replace the dumyData with the data acquired from the API

--- ccg_frontend/ccg_mobile/screens/NavigationScreen.js
+++ ccg_frontend/ccg_mobile/screens/NavigationScreen.js
@@ -4,9 +4,69 @@ import MapView from 'react-native-maps';
 
 import NavigationModes from '../components/navigation/NavigationModes';
 import NavigationInfo from '../components/navigation/NavigationInfo';
+import NavigationMap from '../components/navigation/NavigationMap';
 const NavigationScreen = ({ navigation, route }) => {
     const startPoint = route.params.start;
     const destinationPoint = route.params.destination;
+
+    const dumyData={
+        "total_distance": 6406.1,
+        "total_duration": 4612.2,
+        "steps": [
+          {
+            "distance": 43.1,
+            "duration": 31.0,
+            "instruction": "Head southwest on Rue Sainte-Catherine Ouest",
+            "type": 11,
+            "coordinates": [
+              [
+                -73.57815,
+                45.494763
+              ],
+              [
+                -73.578499,
+                45.494463
+              ]
+            ]
+          },
+          {
+            "distance": 9.7,
+            "duration": 7.0,
+            "instruction": "Turn right",
+            "type": 1,
+            "coordinates": [
+              [
+                -73.578499,
+                45.494463
+              ],
+              [
+                -73.578597,
+                45.494517
+              ]
+            ]
+          },
+          {
+            "distance": 10.1,
+            "duration": 7.3,
+            "instruction": "Turn left",
+            "type": 0,
+            "coordinates": [
+              [
+                -73.578597,
+                45.494517
+              ],
+              [
+                -73.578641,
+                45.494483
+              ],
+              [
+                -73.578683,
+                45.49445
+              ]
+            ]
+          }
+        ]
+    }
 
     // TODO: Implement the navigation logic. API call to backend for directions
 
@@ -15,7 +75,8 @@ const NavigationScreen = ({ navigation, route }) => {
             <NavigationInfo startAddress={startPoint.civic_address} destinationAddress={destinationPoint.civic_address} 
             onBackPress={() => navigation.goBack()}
             />
-            <MapView style={styles.map} />
+            {/*<MapView style={styles.map} />*/}
+            <NavigationMap start={startPoint} destination={destinationPoint} pathCoordinates={dumyData.steps}/>
             <NavigationModes />
         </View>
     );
