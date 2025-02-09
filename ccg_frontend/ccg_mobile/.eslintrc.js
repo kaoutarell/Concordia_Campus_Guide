b58e8db6ef4560845da7646ff103f889284cb4db
Ayaan Jilani <dhakkadchandpandey@gmail.com>
2025-02-09 16:58:53 -0500

    remove typo

--- ccg_frontend/ccg_mobile/.eslintrc.js
+++ ccg_frontend/ccg_mobile/.eslintrc.js
@@ -8,7 +8,6 @@ module.exports = {
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
-    "airbnb",
   ],
   parserOptions: {
     ecmaFeatures: {
