d2fc498982006be50c05f8fa9a889867efd479a7
Ayaan Jilani <dhakkadchandpandey@gmail.com>
2025-02-09 16:50:20 -0500

    back to eslint

---
+++ ccg_frontend/ccg_mobile/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "airbnb",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "react/react-in-jsx-scope": "off", // Not needed in React Native
+    "no-console": "warn", // Warns for console.log but doesn’t fail
+  },
+};
