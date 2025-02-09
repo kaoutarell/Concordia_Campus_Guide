fd6741b59ca3a7dbcb2975a80464b462f79fabd8
Mohadgb <saidimoha.pro@gmail.com>
2025-02-08 20:00:14 -0500

    Refactoring

--- ccg_frontend/ccg_mobile/components/navigation/NavigationInfo.js
+++ ccg_frontend/ccg_mobile/components/navigation/NavigationInfo.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, Dimensions, Animated, SafeAreaView } from 'react-native';
 import { FontAwesome5 } from '@expo/vector-icons';
-
-const { width } = Dimensions.get("window");
+import styles from "../../styles/NavigationInfoStyles";
 
 const NavigationInfo = ({ onStartNavigation, totalDuration, totalDistance }) => {
     const [fadeIn] = React.useState(new Animated.Value(0));
 
     React.useEffect(() => {
-        
         Animated.timing(fadeIn, {
             toValue: 1,
             duration: 600,  
@@ -36,63 +34,5 @@ const NavigationInfo = ({ onStartNavigation, totalDuration, totalDistance }) => 
         </SafeAreaView>
     );
 };
-
-const styles = StyleSheet.create({
-    safeArea: {
-        flex: 1,
-        justifyContent: 'flex-end',
-        backgroundColor: '#800020',  
-    },
-    container: {
-        position: 'absolute',
-        bottom: 0,
-        left: 0,
-        right: 0,
-        backgroundColor: '#800020',  
-        paddingVertical: 12,
-        paddingHorizontal: 20,
-        borderTopLeftRadius: 15,
-        borderTopRightRadius: 15,
-        alignItems: 'center',
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: -4 },
-        shadowOpacity: 0.2,
-        shadowRadius: 8,
-        elevation: 5,
-    },
-    infoBand: {
-        width: '100%',
-        marginBottom: 10,
-    },
-    infoItem: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        marginVertical: 6,
-        justifyContent: 'flex-start',
-    },
-    infoText: {
-        fontSize: 14,
-        fontWeight: '500',
-        color: '#fff',  
-        marginLeft: 8,
-        maxWidth: width - 80,
-    },
-    startButton: {
-        backgroundColor: '#fff',  
-        paddingVertical: 10,
-        paddingHorizontal: 30,
-        borderRadius: 30,
-        alignItems: 'center',
-        width: '100%',
-        maxWidth: 280,
-        marginTop: 10,
-        marginBottom: 30,
-    },
-    startButtonText: {
-        fontSize: 14,
-        fontWeight: 'bold',
-        color: '#800020',  
-    },
-});
 
 export default NavigationInfo;
