614f82ad22771407a59876e827c15f9e4244738c
unknown <z_badawy@live.concordia.ca>
2025-02-12 18:43:24 -0500

    added django secret key to pipeline

--- ccg_backend/concordia_campus_guide_backend/settings.py
+++ ccg_backend/concordia_campus_guide_backend/settings.py
@@ -12,8 +12,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+# Try to get the SECRET_KEY from .env file, if not found, get it from the environment variables
+SECRET_KEY = config('SECRET_KEY', default=os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
