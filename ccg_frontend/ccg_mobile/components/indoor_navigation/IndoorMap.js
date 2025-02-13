2c7c586c50df2c18a89a517dac9d5f5ffa9dcd58
Vega64 <omar.hammodan@gmail.com>
2025-02-13 16:54:49 -0500

    Can display an svg path on the map

--- ccg_frontend/ccg_mobile/components/indoor_navigation/IndoorMap.js
+++ ccg_frontend/ccg_mobile/components/indoor_navigation/IndoorMap.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Text, View, Image, StyleSheet } from 'react-native';
 import Hall8 from './Hall-8.png';
 import { ReactNativeZoomableView } from '@openspacelabs/react-native-zoomable-view';
+import Svg, {Path} from 'react-native-svg';
 
 
 
@@ -11,6 +12,7 @@ const IndoorMap = ()=>{
     <View style={styles.container}>
       <Text>ReactNativeZoomableView</Text>
       <View style={{ borderWidth: 5, height: 400, width: '100%' }}>
+        {/* New component that allows us to zoom and pan the image */}
         <ReactNativeZoomableView
           maxZoom={10}
           minZoom={1}
@@ -23,6 +25,13 @@ const IndoorMap = ()=>{
             style={{ width: '100%', height: '100%', resizeMode: 'contain' }}
             source={Hall8}
           />
+          <Svg style={styles.svg}>
+        <Path
+          d="M 50,150 L 150,50 350,50 450,150"
+          fill="transparent"
+          stroke="red"
+          strokeWidth="5"
+        /></Svg>
         </ReactNativeZoomableView>
       </View>
     </View>
@@ -42,6 +51,13 @@ const styles = StyleSheet.create({
     width: 60,
     height: 60,
     marginVertical: 20,
+  },
+  svg: {
+    position: 'absolute', // Make sure it's positioned on top of the image
+    top: 0,
+    left: 0,
+    width: "100%", // Match the image width
+    height: 300, // Match the image height
   },
 });
 
