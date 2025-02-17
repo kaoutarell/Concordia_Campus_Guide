7bfee8b896df500ac2afb268ee75b4bd199f8ec4
unknown <z_badawy@live.concordia.ca>
2025-02-16 22:07:51 -0500

    created model for shuttle stop and schedule

---
+++ ccg_backend/mapengine/models/shuttle.py
@@ -0,0 +1,17 @@
+from django.contrib.gis.db import models
+
+class ShuttleStop(models.Model):
+    name = models.CharField(max_length=100)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+class ShuttleSchedule(models.Model):
+    stop = models.ForeignKey(ShuttleStop, on_delete=models.CASCADE)
+    day_of_week = models.IntegerField(choices=[(i, i) for i in range(7)])
+    time = models.TimeField(default="00:00:00")
+
+    def __str__(self):
+        return f"{self.stop.name} - {self.day_of_week}"
