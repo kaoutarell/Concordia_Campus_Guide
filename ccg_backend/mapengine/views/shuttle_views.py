b0c19464daa02aab468369f6862775363d2caf00
unknown <z_badawy@live.concordia.ca>
2025-02-17 21:53:43 -0500

    upcoming shuttle using user's coordinates instead of campus name

--- ccg_backend/mapengine/views/shuttle_views.py
+++ ccg_backend/mapengine/views/shuttle_views.py
@@ -3,10 +3,12 @@ from rest_framework.decorators import api_view
 from django.views.decorators.http import require_http_methods
 from ..models.shuttle import ShuttleStop, ShuttleSchedule
 from datetime import datetime
+from django.db.models import F
+from django.db.models.functions import Power
 
 @api_view(['GET'])
 @require_http_methods(['GET'])
-def get_shuttle_stops(request):
+def get_shuttle_stops(_):
     stops = ShuttleStop.objects.all()
     return JsonResponse({
         "stops": [{"name": stop.name, "latitude": stop.latitude, "longitude": stop.longitude} for stop in stops]
@@ -15,18 +17,27 @@ def get_shuttle_stops(request):
 @api_view(['GET'])
 @require_http_methods(['GET'])
 def get_upcoming_sheduled_shuttle(request):
-    campus = request.GET.get('campus')
+    # fix unit tests
+    try:
+        longitude = float(request.GET.get('long', 0.0))
+        latitude = float(request.GET.get('lat', 0.0))
+    except ValueError:
+        longitude = None
+        latitude = None
 
-    if not campus:
-        return JsonResponse({"error": "Missing campus parameter"}, status=400)
-    campus = campus.upper()
-    if not ShuttleStop.objects.filter(name=campus).first():
-        return JsonResponse({"error": "Invalid campus name"}, status=400)
+    if not longitude or not latitude:
+        # default to SGW
+        latitude = ShuttleStop.objects.get(name="SGW").latitude
+        longitude = ShuttleStop.objects.get(name="SGW").longitude
+
+
+    # find the closest shuttle stop
+    closest_stop = ShuttleStop.objects.annotate(distance=Power(F('latitude') - latitude, 2) + Power(F('longitude') - longitude, 2)).order_by('distance').first()
 
     day_of_week = datetime.today().weekday()
     current_time = datetime.now().time()
 
-    schedule = ShuttleSchedule.objects.filter(campus=campus, day_of_week=day_of_week, time__gte=current_time).order_by('time')
+    schedule = ShuttleSchedule.objects.filter(campus=closest_stop.name, day_of_week=day_of_week, time__gte=current_time).order_by('time')
 
     if len(schedule) > 3:
         schedule = schedule[:3]
@@ -41,5 +52,10 @@ def get_upcoming_sheduled_shuttle(request):
         })
 
     return JsonResponse({
+        "shuttle_stop": {
+            "name": closest_stop.name,
+            "latitude": closest_stop.latitude,
+            "longitude": closest_stop.longitude
+        },
         "upcoming_shuttles": upcoming_shuttles
     })
