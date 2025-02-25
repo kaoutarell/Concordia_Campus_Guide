9dd14084ea62f58053d52ec6b08dc03feee1ad9a
unknown <z_badawy@live.concordia.ca>
2025-02-25 15:38:58 -0500

    added POI to admin.py

--- ccg_backend/mapengine/admin.py
+++ ccg_backend/mapengine/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Building, Route, Department, Service, ShuttleSchedule, ShuttleStop
+from .models import Building, Route, Department, Service, ShuttleSchedule, ShuttleStop, PointOfInterest
 
 admin.site.register(Building)
 admin.site.register(Route)
@@ -7,5 +7,6 @@ admin.site.register(Department)
 admin.site.register(Service)
 admin.site.register(ShuttleSchedule)
 admin.site.register(ShuttleStop)
+admin.site.register(PointOfInterest)
 
 # Register your models here.
