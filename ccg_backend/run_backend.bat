9d106d81d363270dab5e408090cb754a1ce7c414
Yassine_Ibhir <yibhir101@gmail.com>
2025-02-25 14:24:33 -0500

    update run_backend

--- ccg_backend/run_backend.bat
+++ ccg_backend/run_backend.bat
@@ -7,7 +7,7 @@ if "%CONDA_DEFAULT_ENV%"=="myenv" (
     timeout /T 5 /NOBREAK
     python manage.py makemigrations
     python manage.py migrate
-    python manage.py loaddata campus_buildings.json routes.json shuttle_stops.json shuttle_schedule.json
+    python manage.py loaddata campus_buildings.json departments.json services.json routes.json shuttle_stops.json shuttle_schedule.json
     python manage.py runserver 0.0.0.0:8000
 ) else (
     echo Please run "conda activate myenv"
