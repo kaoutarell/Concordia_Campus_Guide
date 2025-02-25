644aeff80fde0f81d35f8f859c4e53f72ec60f11
Vega64 <omar.hammodan@gmail.com>
2025-02-25 16:49:47 -0500

    fixed json format and can load the map

--- ccg_backend/mapengine/views/indoor_directions.py
+++ ccg_backend/mapengine/views/indoor_directions.py
@@ -1,8 +1,16 @@
 import requests
+import json
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 
 @api_view(['GET'])
 def get_indoor_directions(request):
+    get_indoor_path()
     path = {"path_data": "M50,150 C150,50 350,50 450,150"}
     return JsonResponse(path)
+
+def get_indoor_path():
+    with open('mapengine/fixtures/h8.json', 'r') as file:
+        map_data=json.load(file)
+
+    print(map_data["H867"])
