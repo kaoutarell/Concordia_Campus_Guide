dade826b0dd596d63a958bff2504c4e435e4616a
Vega64 <omar.hammodan@gmail.com>
2025-02-26 14:15:20 -0500

    added function to connect room to hallway

--- ccg_backend/mapengine/views/indoor_directions.py
+++ ccg_backend/mapengine/views/indoor_directions.py
@@ -3,14 +3,15 @@ import json
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from collections import deque
+import numpy as np
 
 @api_view(['GET'])
 def get_indoor_directions(request):
-    print(get_indoor_path("H867", "H827"))
+    print(get_node_sequence("H867", "H827"))
     path = {"path_data": "M50,150 C150,50 350,50 450,150"}
     return JsonResponse(path)
 
-def get_indoor_path(start, destination):
+def get_node_sequence(start, destination):
     with open('mapengine/fixtures/h8.json', 'r') as file:
         map_data=json.load(file)
     if start not in map_data or destination not in map_data:
@@ -32,4 +33,22 @@ def get_indoor_path(start, destination):
     
     return None
 
-    print(map_data["H867"])
+#this function returns the closest point in the hallway to the class in order to connect the two graphically
+def get_hallway_class_point(map_data, room):
+    corner1=map_data[room]["connections"][1]
+    corner2=map_data[room]["connections"][2]
+    A=(map_data[corner1]["coordinate"]["x"], map_data[room]["coordinate"]["y"])
+    B=(map_data[corner2]["coordinate"]["x"], map_data[room]["coordinate"]["y"])
+    P=(map_data[room]["coordinate"]["x"], map_data[room]["coordinate"]["y"])
+    
+    A = np.array(A)
+    B = np.array(B)
+    P = np.array(P)
+
+    AB = B - A  # Direction vector of line
+    AP = P - A  # Vector from A to P
+
+    t = np.dot(AP, AB) / np.dot(AB, AB)  # Projection scalar
+    Q = A + t * AB  # Compute closest point
+
+    return tuple(Q)
