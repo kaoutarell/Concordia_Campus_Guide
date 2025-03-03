783016a780548181b445af4ee3b22a4a1cb2ddbb
Ayaan Jilani <dhakkadchandpandey@gmail.com>
2025-03-02 21:48:11 -0500

    Revert "setup posthog"
    This reverts commit 1fdd2e5e8b2dd001ea9647538ee4173804fcc23f.

--- ccg_frontend/ccg_mobile/navigation/AppNavigator.js
+++ ccg_frontend/ccg_mobile/navigation/AppNavigator.js
@@ -5,7 +5,6 @@ import { NavigationContainer } from "@react-navigation/native";
 import HomeScreen from "../components/home-screen-ui/HomeScreen";
 import MapScreen from "../components/map-screen-ui/MapScreen";
 import NavigationScreen from "../components/navigation-screen-ui/NavigationScreen";
-import { PostHogProvider } from "posthog-react-native";
 
 import Sidebar from "../components/map-screen-ui/sections/SideBar";
 import CustomNavSearch from "../components/navigation-screen-ui/CustomNavSearch";
@@ -14,37 +13,28 @@ const Stack = createStackNavigator();
 
 // Stack Navigator inside Drawer
 const StackNavigator = () => {
-  return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="Map" component={MapScreen} />
-      <Stack.Screen name="Navigation" component={NavigationScreen} />
-      <Stack.Screen name="Search" component={CustomNavSearch} />
-    </Stack.Navigator>
-  );
+    return (
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Map" component={MapScreen} />
+            <Stack.Screen name="Navigation" component={NavigationScreen} />
+            <Stack.Screen name="Search" component={CustomNavSearch} />
+        </Stack.Navigator>
+    );
 };
 
 const drawerContent = (props) => <Sidebar {...props} />;
 
 // Main App Navigator (Drawer + Stack)
 export default function AppNavigator() {
-  return (
-    <NavigationContainer>
-      <PostHogProvider
-        apiKey={process.env.EXPO_PUBLIC_POSTHOG_API_KEY}
-        options={{
-          host: process.env.EXPO_PUBLIC_POSTHOG_HOST_URL,
-          autocapture: true,
-          enableSessionRecording: true,
-        }}
-      >
-        <Drawer.Navigator
-          drawerContent={drawerContent}
-          screenOptions={{ headerShown: false }}
-        >
-          <Drawer.Screen name="Main" component={StackNavigator} />
-        </Drawer.Navigator>
-      </PostHogProvider>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Drawer.Navigator
+                drawerContent={drawerContent}
+                screenOptions={{ headerShown: false }}
+            >
+                <Drawer.Screen name="Main" component={StackNavigator} />
+            </Drawer.Navigator>
+        </NavigationContainer>
+    );
 }
