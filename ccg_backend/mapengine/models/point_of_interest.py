95009f47056c5b8477a0d0af4a5c5494d5afa625
unknown <z_badawy@live.concordia.ca>
2025-03-06 09:41:34 -0500

    added default values for all models

--- ccg_backend/mapengine/models/point_of_interest.py
+++ ccg_backend/mapengine/models/point_of_interest.py
@@ -1,8 +1,8 @@
 from django.contrib.gis.db import models
 
 class PointOfInterest(models.Model):
-    name = models.CharField(max_length=100)
-    category = models.CharField(max_length=255)
+    name = models.CharField(max_length=100, default="")
+    category = models.CharField(max_length=255, default="")
     location = models.PointField(srid=4326)
-    campus = models.CharField(max_length=255)
-    civic_address = models.CharField(max_length=255)
+    campus = models.CharField(max_length=255, default="SGW")
+    civic_address = models.CharField(max_length=255, default="")
--- ccg_backend/mapengine/models/shuttle.py
+++ ccg_backend/mapengine/models/shuttle.py
@@ -1,11 +1,11 @@
 from django.contrib.gis.db import models
 
 class ShuttleStop(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, default="")
     latitude = models.FloatField()
     longitude = models.FloatField()
 
 class ShuttleSchedule(models.Model):
-    campus = models.CharField(max_length=100)
-    day_of_week = models.IntegerField(choices=[(i, i) for i in range(7)])
+    campus = models.CharField(max_length=100, default="SGW")
+    day_of_week = models.IntegerField(choices=[(i, i) for i in range(7)], default=0)
     time = models.TimeField(default="00:00:00")
