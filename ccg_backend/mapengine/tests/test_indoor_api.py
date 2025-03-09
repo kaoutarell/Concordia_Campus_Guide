1d08aa58edb6ba69080a37ee3b1e269ccbb7c646
Vega64 <omar.hammodan@gmail.com>
2025-03-09 18:15:29 -0400

    test fix

--- ccg_backend/mapengine/tests/test_indoor_api.py
+++ ccg_backend/mapengine/tests/test_indoor_api.py
@@ -1,12 +1,14 @@
 import json
-
+import os
 import pytest
 
 from ..utils.indoor_direction_api_utils import get_pins, get_floor_sequence, get_hallway_class_point, get_node_sequence, get_path_coordinates, convert_coords_to_output, select_map
 
 
+FIXTURE_PATH = os.path.join(os.path.dirname(__file__), "../fixtures/H8.json")
+
 def test_get_pins():
-    with open("mapengine/fixtures/H8.json", "r") as file:
+    with open(FIXTURE_PATH, "r") as file:
         map_data = json.load(file)
     pins = get_pins(map_data, "H867", "H837", False)
     assert pins == [[75, 105], [640, 900]]
@@ -15,7 +17,7 @@ def test_get_pins():
 
 
 def test_get_node_sequence():
-    with open("mapengine/fixtures/H8.json", "r") as file:
+    with open(FIXTURE_PATH, "r") as file:
         map_data = json.load(file)
     sequence = get_node_sequence(map_data, "H867", "H813")
     assert sequence == ["H867", "C1", "C2", "H813"]
@@ -24,14 +26,14 @@ def test_get_node_sequence():
 
 
 def test_get_hallway_class_point():
-    with open("mapengine/fixtures/H8.json", "r") as file:
+    with open(FIXTURE_PATH, "r") as file:
         map_data = json.load(file)
     point = get_hallway_class_point(map_data, "H813")
     assert point == (765, 220)
 
 
 def test_get_path_coordinates():
-    with open("mapengine/fixtures/H8.json", "r") as file:
+    with open(FIXTURE_PATH, "r") as file:
         map_data = json.load(file)
     sequence = ["H867", "C1", "C2", "H813"]
     coords = get_path_coordinates(map_data, sequence)
@@ -59,7 +61,7 @@ def test_convert_coords_to_output():
 
 
 def test_select_map():
-    with open("mapengine/fixtures/H8.json", "r") as file:
+    with open(FIXTURE_PATH, "r") as file:
         map_data = json.load(file)
     assert map_data == select_map("H8")
     assert select_map("doesntexist") is None
