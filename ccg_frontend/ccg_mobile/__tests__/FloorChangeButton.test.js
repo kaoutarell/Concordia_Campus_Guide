8a8ed3a4cb118b165696a0bd3cf331f4a0fbe81f
Vega64 <omar.hammodan@gmail.com>
2025-03-17 06:24:14 -0400

    added more tests to the floor change button component

--- ccg_frontend/ccg_mobile/__tests__/FloorChangeButton.test.js
+++ ccg_frontend/ccg_mobile/__tests__/FloorChangeButton.test.js
@@ -8,8 +8,30 @@ describe("IndoorMap Component", () => {
     expect(getByTestId("prev-button")).toBeTruthy();
   });
 
+  it("calls onPress function when pressed", () => {
+    const mockOnPress = jest.fn();
+    const { getByTestId } = render(<FloorChangeButton index={1} maxIndex={5} setIndex={mockOnPress} />);
+
+    // Press the button
+    fireEvent.press(getByTestId("prev-button"));
+
+    // Check if onPress was called
+    expect(mockOnPress).toHaveBeenCalled();
+  });
+
   it("displays next button", () => {
     const { getByTestId } = render(<FloorChangeButton />);
     expect(getByTestId("next-button")).toBeTruthy();
+  });
+
+  it("calls onPress function when pressed", () => {
+    const mockOnPress = jest.fn();
+    const { getByTestId } = render(<FloorChangeButton index={1} maxIndex={5} setIndex={mockOnPress} />);
+
+    // Press the button
+    fireEvent.press(getByTestId("next-button"));
+
+    // Check if onPress was called
+    expect(mockOnPress).toHaveBeenCalled();
   });
 });
