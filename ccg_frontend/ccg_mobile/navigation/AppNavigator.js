20c7e9d8b1039c587956c77da561cc7998560e19
unknown <z_badawy@live.concordia.ca>
2025-03-17 17:59:27 -0400

    set ms clarity for usability testing

--- ccg_frontend/ccg_mobile/navigation/AppNavigator.js
+++ ccg_frontend/ccg_mobile/navigation/AppNavigator.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, useNavigationContainerRef } from "@react-navigation/native";
 import HomeScreen from "../components/home-screen-ui/HomeScreen";
 import MapScreen from "../components/map-screen-ui/MapScreen";
 import NavigationScreen from "../components/navigation-screen-ui/NavigationScreen";
@@ -9,6 +9,7 @@ import NavigationScreen from "../components/navigation-screen-ui/NavigationScree
 import Sidebar from "../components/map-screen-ui/sections/SideBar";
 import CustomNavSearch from "../components/navigation-screen-ui/CustomNavSearch";
 import IndoorScreen from "../screens/IndoorScreen";
+import * as Clarity from '@microsoft/react-native-clarity';
 
 const Drawer = createDrawerNavigator();
 const Stack = createStackNavigator();
@@ -27,11 +28,35 @@ const StackNavigator = () => {
 };
 
 const drawerContent = props => <Sidebar {...props} />;
+const projectID = process.env.EXPO_PUBLIC_CLARITY_PROJECT_ID;
 
 // Main App Navigator (Drawer + Stack)
 export default function AppNavigator() {
+  const navigationRef = useNavigationContainerRef();
+  const routeNameRef = React.useRef();
+
   return (
-    <NavigationContainer>
+    <NavigationContainer
+      ref={navigationRef}
+      onReady={() => {
+        routeNameRef.current = navigationRef.getCurrentRoute().name;
+        const clarityConfig = {
+          logLevel: Clarity.LogLevel.Verbose
+        };
+
+        Clarity.initialize(projectID, clarityConfig);
+        Clarity.setCurrentScreenName(routeNameRef.current);
+      }}
+      onStateChange={() => {
+        const previousRouteName = routeNameRef.current;
+        const currentRouteName = navigationRef.getCurrentRoute().name;
+
+        if (previousRouteName !== currentRouteName) {
+          routeNameRef.current = currentRouteName;
+          Clarity.setCurrentScreenName(currentRouteName);
+        }
+      }}
+    >
         <Drawer.Navigator drawerContent={drawerContent} screenOptions={{ headerShown: false }}>
           <Drawer.Screen name="Main" component={StackNavigator} />
         </Drawer.Navigator>
