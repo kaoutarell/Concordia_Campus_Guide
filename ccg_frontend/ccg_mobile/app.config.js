925bd3dd8671087716955128ce0ab415f83655d8
unknown <z_badawy@live.concordia.ca>
2025-03-18 21:58:26 -0400

    prettier format

--- ccg_frontend/ccg_mobile/app.config.js
+++ ccg_frontend/ccg_mobile/app.config.js
@@ -1,52 +1,47 @@
-import 'dotenv/config';
+import "dotenv/config";
 
-export default ({config}) => (
-    {
-        "expo": {
-          "name": "ccg_mobile",
-          "slug": "ccg_mobile",
-          "version": "1.0.0",
-          "orientation": "portrait",
-          "icon": "./assets/icon.png",
-          "userInterfaceStyle": "light",
-          "newArchEnabled": true,
-          "splash": {
-            "image": "./assets/splash-icon.png",
-            "resizeMode": "contain",
-            "backgroundColor": "#ffffff"
-          },
-          "plugins": [
-            [
-              "expo-location",
-              {
-                "locationAlwaysAndWhenInUsePermission": "Allow the app to access your location at all times.",
-                "locationWhenInUsePermission": "Allow the app to access your location while using the app."
-              }
-            ],
-            "expo-localization"
-          ],
-          "ios": {
-            "supportsTablet": true
-          },
-          "android": {
-            "adaptiveIcon": {
-              "foregroundImage": "./assets/adaptive-icon.png",
-              "backgroundColor": "#ffffff"
-            },
-            "permissions": [
-              "android.permission.ACCESS_COARSE_LOCATION",
-              "android.permission.ACCESS_FINE_LOCATION"
-            ],
-            "config": {
-              "googleMaps": {
-                "apiKey": process.env.GOOGLE_MAPS_API_KEY
-              }
-            },
-            "package": "com.ikozay.ccg_mobile"
-          },
-          "web": {
-            "favicon": "./assets/favicon.png"
-          }
-        }
-      }
-);
+export default ({ config }) => ({
+  expo: {
+    name: "ccg_mobile",
+    slug: "ccg_mobile",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    newArchEnabled: true,
+    splash: {
+      image: "./assets/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    plugins: [
+      [
+        "expo-location",
+        {
+          locationAlwaysAndWhenInUsePermission: "Allow the app to access your location at all times.",
+          locationWhenInUsePermission: "Allow the app to access your location while using the app.",
+        },
+      ],
+      "expo-localization",
+    ],
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+      permissions: ["android.permission.ACCESS_COARSE_LOCATION", "android.permission.ACCESS_FINE_LOCATION"],
+      config: {
+        googleMaps: {
+          apiKey: process.env.GOOGLE_MAPS_API_KEY,
+        },
+      },
+      package: "com.ikozay.ccg_mobile",
+    },
+    web: {
+      favicon: "./assets/favicon.png",
+    },
+  },
+});
